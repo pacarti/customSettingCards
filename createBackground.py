19d08e52a513deb454cd0dda2c8ba457851c0ef9
pacarti <a.paciorkowski@protonmail.com>
2025-02-09 23:59:14 +0100

    Works as expected

---
+++ createBackground.py
@@ -0,0 +1,26 @@
+import os
+from PIL import Image, ImageDraw
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+bg = Image.new('RGBA', (384, 480), 'white')
+
+# bg.save('background.png')
+
+# exit()
+
+# Add a guidelines to cut(black frame)
+
+bgCopy = bg.copy()
+
+width, height = bgCopy.size
+
+draw = ImageDraw.Draw(bgCopy)
+
+draw.rectangle((10, 10, width-10, height-10), fill='white', outline='black')
+
+flowers = Image.open('invTempSmall.png')
+
+bgCopy.paste(flowers, (0, 0), flowers)
+
+bgCopy.save('template.png')
---
+++ customSettingCards.py
@@ -0,0 +1,52 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# Load the names from text file and save them into the list:
+guestsFile = open('guests.txt')
+readGuestsFromFile = guestsFile.readlines()
+
+guests = []
+
+for guest in readGuestsFromFile:
+    guestTrimmed = guest.strip('\n')
+    guests.append(guestTrimmed)
+
+
+# Set the fonts:
+
+antykwaFontFolder = 'fonts/antt'
+
+qtMerryScriptFont = ImageFont.truetype('fonts/QTMerryScript.otf', 16)
+
+# antykwaFont = ImageFont.truetype(os.path.join(antykwaFontFolder, 'AntykwaTorunska-Regular.otf'), 10)
+
+nameFont = ImageFont.truetype(os.path.join(antykwaFontFolder, 'AntykwaTorunska-BoldItalic.otf'), 20)
+
+dateFont = ImageFont.truetype(os.path.join(antykwaFontFolder, 'AntykwaTorunska-Regular.otf'), 20)
+
+# Create an invitation for each guest:
+
+for guest in guests:
+
+    # Load the flower background:
+
+    imTemp = Image.open('template.png')
+    width, height = imTemp.size
+
+    invitation = ImageDraw.Draw(imTemp)
+
+    invitation.text((91,163), 'It would be a pleasure to have the company of', fill='black', font=qtMerryScriptFont)
+    # Improve the centering if the length of the guest name is short:
+    if len(guest) < 10:
+        invitation.text((148,200), guest, fill='black', font=nameFont)
+    else:
+        invitation.text((138,200), guest, fill='black', font=nameFont)
+    invitation.text((91,230), 'at 11010 Memory Lane on the Evening of', fill='black', font=qtMerryScriptFont)
+    invitation.text((158,260), '1st April', fill='black', font=dateFont)
+    invitation.text((168,290), 'at 7 o\'clock', fill='black', font=qtMerryScriptFont)
+
+    imTemp.save('invitation for %s.png' % guest)
+
+    imTemp.close()
